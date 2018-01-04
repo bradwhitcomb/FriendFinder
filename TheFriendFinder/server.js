704d2d57b527cef96432ae84a6df3c6b8bf8b22d
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-01-04 07:07:25 -0500

    logic works but modal pop-up doesn't

--- TheFriendFinder/server.js
+++ TheFriendFinder/server.js
@@ -7,8 +7,8 @@ var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
-
+var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || 8080;
 
 
 console.log("hello world");
